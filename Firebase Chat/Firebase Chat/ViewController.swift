f5a0bfda5b2d2ff064476671bfc0a225233d1482
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-04-26 22:59:32 -0400

    Initial Commit

---
+++ Firebase Chat/Firebase Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Chat
+//
+//  Created by Christy Hicks on 4/26/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
